9f93562a4e43cd8b112189911b9fd7d5b81f8b65
Insane Eagle <96535338+InsaneEagle@users.noreply.github.com>
2022-05-13 09:29:10 +0000

    Added more status

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,15 @@
 module.exports = (client) => {
 console.log((`Successfully Logged In As ${client.user.tag}\n\n`));
 
-  const activities = [`Giveaways in ${client.guilds.cache.size} guilds`,"g.help",`over ${client.users.cache.size} users!`];
   setInterval(() => {
-    let activity = activities[Math.floor(Math.random() * activities.length)];
-    client.user.setActivity("Giveaways by: dsc.gg/nxtlvedevs", { type: "WATCHING" });
-  }, 20000);
+      const statuses = [
+        `g.help by: dsc.gg/nxtlvedevs`,
+        `Giveaway's by: dsc.gg/nxtlvedevs`,
+        `24/7 Working by: dsc.gg/nxtlvedevs`,
+        `Best Bots by: dsc.gg/nxtlvedevs`,
+      ];
+      const status = statuses[Math.floor(Math.random() * statuses.length)];
+      client.user.setActivity(status, { type: "WATCHING" });
+    }, 60000);
 
 };
